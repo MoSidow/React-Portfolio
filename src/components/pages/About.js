6fddf2491a361db5ab5087ccbc1c68386f9c36f2
Mohamed Sidow <sidowmohamed35@gmail.com>
2023-03-06 20:49:30 +0000

    finished contact page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,9 +3,10 @@ import avatar from'../images/20220715_190418 (1).jpg';
 
 export default function About() {
   return (
+    <div>
+    <h2 className='portfolio'>About Me</h2>
     <div className='back'>
     <div className='aboutme'>
-    <h2>About Me</h2>
     <p>Hi 👋 My name is <span className='name'>Mohamed</span><br/><br/> <img src={avatar} alt='avatar'></img><br/><br/>
     (You can call me Mo Sidow)
        I live In Birmingham, West Midlands.
@@ -14,6 +15,7 @@ export default function About() {
     I took this Course, because I've always wanted to know what really is behind in Creating a website and I have an eager to learn how it is done, So I can hopefully get into a Career of Web development.
   Fun Fact: I enjoy Playing Football</p>
  </ div>
+ </div>
  </div>
   );
 }
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,20 +3,27 @@ import React from 'react';
 export default function Contact() {
   return (
     <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <h2 className='portfolio'>Contact Me</h2>
+      <form className='form'>
+          <div>
+            <label className='Ftitle'>Name:</label>
+            <input type="text" required placeholder='Enter Your Name' className='input'/>
+          </div>
+
+          <div>
+            <label className='Ftitle'>Email:</label>
+            <input required type='email' placeholder='Enter Your Email' className='input'/>
+          </div>
+
+          <div>
+            <label className='Ftitle'>Message:</label>
+            <textarea required placeholder='Enter Your Message' className='input'/>
+          </div>
+
+          <div>
+            <button className='button1'>Submit Form</button>
+          </div>
+        </form>
     </div>
   );
 }
