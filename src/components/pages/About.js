024c19c4c4e7666bb56f6ef8bf745a72909cb765
Mohamed Sidow <sidowmohamed35@gmail.com>
2023-06-08 14:36:53 +0100

    updated about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -9,11 +9,7 @@ export default function About() {
     <div className='aboutme'>
     <p>Hi 👋 My name is <span className='name'>Mohamed</span><br/><br/> <img src={avatar} alt='avatar'></img><br/><br/>
     (You can call me Mo Sidow)
-       I live In Birmingham, West Midlands.
-       My highest level of education is A levels.
-      IT Skills probably 8 out of 10. I am currently studying Full Stack Web development at Birmingham City University, graduating this month.
-    I took this Course, because I've always wanted to know what really is behind in Creating a website and I have an eager to learn how it is done, So I can hopefully get into a Career of Web development.
-  Fun Fact: I enjoy Playing Football</p>
+    As a graduate from University of Birmingham in an intensive 24-week Full Stack Web Development course I have gained the skills and confidence required in working and collaborating with a team to produce outstanding coding projects. An individual with great problem solving skills that contributes to the team for an outcome of success. I have a passion for always learning as I advance further into my Coding career.</p>
  </ div>
  </div>
  </div>
